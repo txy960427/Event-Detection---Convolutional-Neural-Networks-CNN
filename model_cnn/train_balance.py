8cb00d50f0ff33a67fc587fb117616bd6669d2c2
txy960427 <sweet_402392170@qq.com>
2019-06-05 18:38:50 +0800

    Update train_balance.py

--- model_cnn/train_balance.py
+++ model_cnn/train_balance.py
@@ -12,32 +12,43 @@ from  sklearn.metrics import precision_score
 from  sklearn.metrics import f1_score
 from gensim.models.keyedvectors import KeyedVectors
 import random
-
-lr = 0.005
+from sklearn.metrics import classification_report
+lr = 0.001
 window_size=31
-batch_size=2056
-filter_size=150
+batch_size=8000
+filter_size=200
 embedding_size=300
 class_number=34
 filter_sizes='2,3,4,5'
-l2_lambad=3.0
+l2_lambad=3
 drop_keep_out=0.5
-epochs=600
+epochs=2000
+np.random.seed(4567)
 pretrain_emb_path="../GoogleNews-vectors-negative300.bin"
 
 def getava(true_label,prediction_label):
     real_trigger_number = 0
+    real_trigger_label=[]
+    pre_trigger_label=[]
     for tt in true_label:
         if tt != 0:
             real_trigger_number = real_trigger_number + 1
+            real_trigger_label.append(tt)
     pre_trigger_number = 0
-    for tt in prediction_label:
+    pre_trigger_number_index=[]
+    wrong_trigger_number_index=[]
+
+    for i,tt in enumerate(prediction_label):
         if tt != 0:
             pre_trigger_number = pre_trigger_number + 1
+            pre_trigger_label.append(tt)
     true_number = 0
     for i in range(len(true_label)):
         if true_label[i] != 0 and true_label[i] == prediction_label[i]:
             true_number = true_number + 1
+            pre_trigger_number_index.append(i)
+        elif true_label[i] != 0 and true_label[i] != prediction_label[i]:
+            wrong_trigger_number_index.append(i)
     if pre_trigger_number != 0:
         precision = true_number / pre_trigger_number
     else:
@@ -50,8 +61,9 @@ def getava(true_label,prediction_label):
         f1 = 0
     else:
         f1 = 2 * precision * recall / (precision + recall)
-   # print(real_trigger_number,pre_trigger_number,true_number)
-    return precision, recall, f1
+    #print(real_trigger_number,pre_trigger_number,true_number)
+   # print(classification_report(true_label, prediction_label))
+    return precision, recall, f1,pre_trigger_number_index,wrong_trigger_number_index,real_trigger_label,pre_trigger_label
 
 
 def getlabel(predictions,labels):
@@ -89,13 +101,16 @@ def loadEmbMatrix(pretrain_emb_path,text,embed_size):
     embedding_matrix = np.zeros((len(text), embed_size), dtype='float32')
     word_dict = {}
     for i, tt in enumerate(text):
-        if i % 3000 == 0:
-            print(i)
-        try:
-            word_dict[tt] = i
-            embedding_matrix[i] = model[tt]
-        except KeyError:
-            print(tt)
+        if tt=='None':
+            word_dict[tt]=i
+        else:
+            if i % 3000 == 0:
+                print(i)
+            try:
+                word_dict[tt] = i
+                embedding_matrix[i] = model[tt]
+            except KeyError:
+                continue
 
     return embedding_matrix, word_dict
 
@@ -104,9 +119,10 @@ def main():
     train_sentences, test_sentences, dev_sentences, train_trigger_labels, test_trigger_labels, dev_trigger_labels, \
 	train_entity_labls, test_entity_labls, dev_entity_labls,\
     train_sequences,test_sequences,dev_sequences,\
-    train_positions,tset_positions,dev_positions,\
+    train_positions,test_positions,dev_positions,\
     train_entitys,test_entitys,dev_entitys,\
     train_labels,test_labels,dev_labels=pre.predata()
+
 
     print(len(train_sequences),len(test_sequences),len(dev_sequences))   #[[[w1][w2]...[wn]][][]...[]]
 
@@ -121,6 +137,12 @@ def main():
     #words_new.append('None')
 
     emb_matrix, word_dict = loadEmbMatrix(pretrain_emb_path, words_new, embedding_size)
+    print(len(word_dict))
+
+    emb_matrix2=[]
+    emb_matrix2.append(emb_matrix[0].tolist())
+    print(emb_matrix2)
+
    # print(emb_matrix[0],emb_matrix[1])
 
 
@@ -149,6 +171,7 @@ def main():
             se_sentence.append(se)
         x_test.append(se_sentence)
 
+
     gcn = model.Gcns(  #实际是cnn  懒得改了。
         sequence_length=window_size,
         num_classes=class_number,
@@ -156,17 +179,20 @@ def main():
         embedding_size=embedding_size,
         filter_sizes=list(map(int, filter_sizes.split(","))),
         num_filters=filter_size,
-        emb_matrix=emb_matrix,
+        emb_matrix1=emb_matrix[1:],
+        emb_matrix2=emb_matrix2,
         l2_reg_lambda=l2_lambad
     )
 
-    train_op = tf.train.AdamOptimizer(learning_rate=lr,epsilon=1e-6).minimize(gcn.loss)
+
+
+    train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(gcn.loss)
     print(lr)
     sess = tf.InteractiveSession()
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    def train_step(x_batch, position_indexs,entity_indexs,y_batch):
+    def train_step(x_batch, position_indexs,entity_indexs,y_batch,batch_size):
         """
         A single training step
         """
@@ -175,24 +201,27 @@ def main():
             gcn.input_y: y_batch,
             gcn.position_index: position_indexs,
             gcn.entity_index: entity_indexs,
-            gcn.dropout_keep_prob: drop_keep_out
+            gcn.dropout_keep_prob: drop_keep_out,
+            gcn.batch_size:batch_size,
         }
-        _, loss, accuracy, predictions = sess.run(
-            [train_op, gcn.loss, gcn.accuracy, gcn.predictions], feed_dict)
-        return loss, accuracy, predictions
+        _, loss, accuracy, predictions,scores,W_train = sess.run(
+            [train_op, gcn.loss, gcn.accuracy, gcn.predictions,gcn.pred_probas,gcn.W], feed_dict)
 
-    def dev_step(x_batch, position_indexs,entity_indexs, y_batch):
+        return loss, accuracy, predictions,scores,W_train
+
+    def dev_step(x_batch, position_indexs,entity_indexs, y_batch,batch_size):
 
         feed_dict = {
             gcn.input_x: x_batch,
             gcn.input_y: y_batch,
             gcn.position_index: position_indexs,
             gcn.entity_index:entity_indexs,
-            gcn.dropout_keep_prob: 1.0
+            gcn.dropout_keep_prob:1,
+            gcn.batch_size:batch_size,
         }
-        loss, accuracy, predictions = sess.run([gcn.loss, gcn.accuracy, gcn.predictions], feed_dict)
+        loss, accuracy, predictions,scores = sess.run([gcn.loss, gcn.accuracy, gcn.predictions,gcn.pred_probas], feed_dict)
 
-        return loss, accuracy, predictions
+        return loss, accuracy, predictions,scores
 
     x = []
     x_position = []
@@ -212,7 +241,6 @@ def main():
         for yyy in yy:
             y.append(yyy)
 
-
     x_batch_se_dev = []
     x_position_se_dev = []
     x_entity_se_dev = []
@@ -231,67 +259,139 @@ def main():
         for yyy in yy:
             y_batch_se_dev.append(yyy)
 
+    x_batch_se_test = []
+    x_position_se_test = []
+    x_entity_se_test = []
+    y_batch_se_test = []
+
+    for xx in x_test:
+        for xxx in xx:
+            x_batch_se_test.append(xxx)
+    for xx in test_positions:
+        for xxx in xx:
+            x_position_se_test.append(xxx)
+    for xx in test_entitys:
+        for xxx in xx:
+            x_entity_se_test.append(xxx)
+    for yy in test_labels:
+        for yyy in yy:
+            y_batch_se_test.append(yyy)
+
+    print(len(x),len(x_position),len(x_entity),len(y))
+    print(x[0])
 
     # Generate batches
     batches = batch_iter(
-        list(zip(x,x_position,x_entity,y)), batch_size, epochs)
+        list(zip(x, x_position, x_entity, y)), batch_size, epochs)
     best_f1=0.0
     # Training loop. For each batch...
     for k, batch in enumerate(batches):
        # print(k)
         x_batch,x_position_batch,x_entity_batch,y_batch = zip(*batch)
+       # print(len(x_batch),len(x_position_batch))
 
-        number = []
-        number_trigger = []
-        number_trigger_13 = []
+        number_triggers=[]
+        for i in range(34):
+            number_triggers.append([])
         for i, ll in enumerate(y_batch):
-            if ll == [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0]:
-                number.append(i)
-            else:
-                if ll == [0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0,
-                      0, 1, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0]:
-                    number_trigger_13.append(i)
-                number_trigger.append(i)  # qule13
-       # print(len(number), len(number_trigger), len(number_trigger_13))
+            for j in range(34):
+                label=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0]
+                label[j]=1
+                if ll == label:
+                    number_triggers[j].append(i)
+       # print(number_triggers)
 
         x_balance = []
         x_position_balance = []
         x_entity_balance = []
         y_balance = []
-        random.shuffle(number)
-        for i in range(int(len(number_trigger_13)*2.5)+0):  # 控制非trigger的数量
-            x_balance.append(x_batch[number[i]])
-            x_position_balance.append(x_position_batch[number[i]])
-            x_entity_balance.append(x_entity_batch[number[i]])
-            y_balance.append(y_batch[number[i]])
-        for i in range(len(number_trigger)):  # qu13
-            x_balance.append(x_batch[number_trigger[i]])
-            x_position_balance.append(x_position_batch[number_trigger[i]])
-            x_entity_balance.append(x_entity_batch[number_trigger[i]])
-            y_balance.append(y_batch[number_trigger[i]])
+        np.random.shuffle(number_triggers[0])
+        if number_triggers[13]!=0:
+            for i in range(int(len(number_triggers[13])*2.5)+0):  # 控制非trigger的数量
+                x_balance.append(x_batch[number_triggers[0][i]])
+                x_position_balance.append(x_position_batch[number_triggers[0][i]])
+                x_entity_balance.append(x_entity_batch[number_triggers[0][i]])
+                y_balance.append(y_batch[number_triggers[0][i]])
+            for i in range(34):
+                if i!=0:
+                    try:
+                        if int(len(number_triggers[13])/len(number_triggers[i])/3)==0:
+                            for j in range(len(number_triggers[i])):
+                                x_balance.append(x_batch[number_triggers[i][j]])
+                                x_position_balance.append(x_position_batch[number_triggers[i][j]])
+                                x_entity_balance.append(x_entity_batch[number_triggers[i][j]])
+                                y_balance.append(y_batch[number_triggers[i][j]])
+                        else:
+                            for m in range(int(len(number_triggers[13])/len(number_triggers[i]))):
+                                for j in range(len(number_triggers[i])):
+                                    x_balance.append(x_batch[number_triggers[i][j]])
+                                    x_position_balance.append(x_position_batch[number_triggers[i][j]])
+                                    x_entity_balance.append(x_entity_batch[number_triggers[i][j]])
+                                    y_balance.append(y_batch[number_triggers[i][j]])
+                    except:
+                        continue
+        else:
+            x_balance=x_batch
+            x_position_balance=x_position_batch
+            x_entity_balance=x_entity_batch
+            y_balance=y_batch
 
-      # print(len(x_balance))
 
-        loss_train, acc_train, prediction_bath = train_step(x_balance,x_position_balance,x_entity_balance, y_balance)
 
-        if k % (int(len(x) / batch_size / 2)) == 0:
+        #print(len(x_balance))
 
-            loss_dev, acc_dev, prediction_dev = dev_step(x_batch_se_dev,x_position_se_dev,x_entity_se_dev,y_batch_se_dev)
-            true_label_dev, prediction_label_dev = getlabel(prediction_dev,y_batch_se_dev)
-            prec_dev, recall_dev, f1_dev = getava(true_label_dev, prediction_label_dev)
+        loss_train, acc_train, prediction_bath,scores_train,W_train= train_step(x_balance,x_position_balance,x_entity_balance, y_balance,len(x_balance))
 
-            if f1_dev>best_f1:  #如果f1有提升
+       # print(W_train[0])
+
+        if k % (int(len(x) / batch_size /10)) == 0:
+            if k%500==0:
+                print(k)
+            loss_dev, acc_dev, prediction_dev,scores_dev = dev_step(x_batch_se_dev, x_position_se_dev, x_entity_se_dev,
+                                                         y_batch_se_dev,len(x_batch_se_dev))
+
+
+            true_label_dev, prediction_label_dev = getlabel(prediction_dev, y_batch_se_dev)
+            prec_dev, recall_dev, f1_dev,pre_trigger_dev,wrong_trigger_dev,real_trigger_label_dev,pre_trigger_label_dev = getava(true_label_dev, prediction_label_dev)
+
+            if f1_dev>best_f1 :  # 如果f1有提升
                 best_f1 = f1_dev
-                print("epoch {:g} train loss {:g}, acc {:g}".format( k / (int(len(x) / batch_size / 2)),loss_train, acc_train))
-                print(prediction_label_dev[0:20])
-                print(true_label_dev[0:20])
-                print("dev loss {:g}, acc {:g} precison {:g} recall {:g} f1 {:g}".format(loss_dev, acc_dev, prec_dev,
-                                                                                     recall_dev, f1_dev))
+                if True:
+                    print("epoch {:g} train loss {:g}, acc {:g}".format(k / (int(len(x) / batch_size / 10)),
+                                                                        loss_train,
+                                                                        acc_train))
+#
+                    print("dev loss {:g}, acc {:g} precison {:g} recall {:g} f1 {:g}".format(loss_dev, acc_dev,
+                                                                                             prec_dev,
+                                                                                             recall_dev, f1_dev))
+                    loss_test, acc_test, prediction_test,scores_test = dev_step(x_batch_se_test, x_position_se_test,
+                                                                    x_entity_se_test,
+                                                                    y_batch_se_test,len(x_batch_se_test))
+                    true_label_test, prediction_label_test = getlabel(prediction_test, y_batch_se_test)
+
+                    prec_test, recall_test, f1_test,pre_trigger_test,wrong_trigger_test,real_trigger_label_test,pre_trigger_label_test = getava(true_label_test, prediction_label_test)
+                    if f1_test>0.0:
+
+                        print(
+                        "test loss {:g}, acc {:g} precison {:g} recall {:g} f1 {:g}".format(loss_test, acc_test,
+                                                                                            prec_test,
+                                                                                           recall_test, f1_test))
+
+                        right_words=[]
+                        wrong_words=[]
+
+                        for ind in pre_trigger_test:
+                            right_words.append(list (word_dict.keys()) [list (word_dict.values()).index (x_batch_se_test[ind][15])])
+                        #real_trigger_label_test.append(ind)
+                        for ind in wrong_trigger_test:
+                            wrong_words.append(list (word_dict.keys()) [list (word_dict.values()).index (x_batch_se_test[ind][15])])
+                        #wrong_labels.append()
+                   # print(right_words)
+                       # print(wrong_words)
+                      #  print(classification_report(true_label_test, prediction_label_test))
+
+
 
 
 if __name__ == '__main__':
